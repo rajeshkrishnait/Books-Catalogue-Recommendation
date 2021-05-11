729e31a6a026f8a64b50ce81590da8e1761c2345
Prashanth G <prashanthg321@gmail.com>
2021-05-11 17:47:45 +0530

    files deleted

--- Book_Recommendation/app1.py
+++ Book_Recommendation/app1.py
@@ -18,14 +18,9 @@ def predict(str):
     isbn_number = str
     distances, indices = model.kneighbors(users_combined_rating_pivot.loc[users_combined_rating_pivot['ISBN']==isbn_number,:].values[:,1:].reshape(1,-1), n_neighbors = 6)
     list_books = []
-    for i in range(0, len(distances.flatten())):
-        #if i == 0:
-            #print('Recommendations for {0}:\n'.format(get_book_name(users_combined_rating_pivot.index[query_index])))
-        if i>0:
-            book_num = users_combined_rating_pivot.iloc[indices.flatten()[i]][0]
-            #book_name = get_book_name(book_num)
-            #print('{0}: {1}-{2}, with distance of {3}:'.format(i, book_name, book_num, distances.flatten()[i]))
-            list_books.append(book_num)
+    for i in range(1, len(distances.flatten())):
+        book_num = users_combined_rating_pivot.iloc[indices.flatten()[i]][0]
+        list_books.append(book_num)
     print(list_books)
     sys.stdout.flush()
 
